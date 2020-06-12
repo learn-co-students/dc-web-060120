fadb3665332299e1707e1ce0a96202fcb1e65da5
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2020-06-12 11:17:59 -0400

    before group work

---
+++ 12-intro-to-active-record/db/seeds.rb
@@ -0,0 +1,2 @@
+Author.find_or_create_by(name: "Maya Angelou")
+Author.find_or_create_by(name: "Theodore Geisel", penname: "Dr Seuss")
--- 12-intro-to-active-record/lib/author.rb
+++ 12-intro-to-active-record/lib/author.rb
@@ -1,3 +1,28 @@
 class Author < ActiveRecord::Base
     
+    def self.create(name: nil, penname: nil)
+        puts "New author has been created"
+
+        super({name: name, penname: penname})
+
+    end
 end
+
+
+
+
+# module ActiveRecord
+
+#     class Base
+
+#         def self.all 
+#             # retrieves all from database
+#         end
+
+#         def self.create
+#             # creates the object and saves to db
+#         end
+
+#     end
+
+# end
