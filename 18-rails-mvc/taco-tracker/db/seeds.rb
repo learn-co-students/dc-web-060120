fe796b975b2afa4137a4fc64df10e2fbaca484c6
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2020-06-24 09:05:29 -0400

    fixed bug in seeds

--- 18-rails-mvc/taco-tracker/db/seeds.rb
+++ 18-rails-mvc/taco-tracker/db/seeds.rb
@@ -8,7 +8,7 @@
 Taco.destroy_all
 
 def random_boolean
-    return rand(1.0) > 0.5
+    return rand(0..1.0) > 0.5
 end
 
-20.times {Taco.create(name: "#{Faker::Hipster.word} Taco", price: rand(1.0...10.0), vegetarian: random_boolean)}
+20.times {Taco.create(name: "#{Faker::Hipster.word} Taco", price: rand(1.0...10.0).round(2), vegetarian: random_boolean)}
