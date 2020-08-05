58fdd04b6e6575b7857eb84838cb0e01661cd190
vidhisharma3193 <vidhisharma3193@gmail.com>
2020-08-05 11:06:10 -0500

    done

--- 49-State-forms/src/painting.js
+++ 49-State-forms/src/painting.js
@@ -11,6 +11,19 @@ class Painting extends React.Component{
 
     //TASK: have a state for votes for the painting and update it when div with class "ui red button" clicked
 
+    constructor(props){
+        super(props)
+        this.state={
+            votes: props.painting.votes
+        }
+    }
+
+    addVote = () => {
+        this.setState({
+            votes: ++this.state.votes
+        })
+    } 
+
     render(){
         // console.log(this) => class
         return(<div>
@@ -18,11 +31,11 @@ class Painting extends React.Component{
             <img src={this.props.painting.image} />
             <br />
             <div class="ui labeled button" tabindex="0">
-                <div class="ui red button">
+                <div class="ui red button" onClick={this.addVote}>
                     <i class="heart icon"></i> Vote
                 </div>
                 <a class="ui basic red left pointing label">
-                    {this.props.painting.votes}
+                    {this.state.votes}
                 </a>
             </div>
             <br/>
